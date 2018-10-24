e024485ae20a269001e7e97fec794b19bcd92645
AliceEMaffs <32435141+AliceEMaffs@users.noreply.github.com>
2018-10-24 18:54:31 +0100

    Update Zmumu2Jets_Analysis.h

--- Code/Headers/Zmumu2Jets_Analysis.h
+++ Code/Headers/Zmumu2Jets_Analysis.h
@@ -233,26 +233,26 @@ void MC_Analysis::Zmumu2Jets_DrawHistos() {
 	// DrawHistogram(histogram, canvas name, histogram name, x axis title, canvas x size, canvas y size, bool for log y axis, output file name)
 
 	// ptcone histograms
-	DrawHistogram(h_muon_0_iso_ptcone20, "h_muon_0_iso_ptcone20", "h_muon_0_iso_ptcone20_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptcone20.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_iso_ptcone30, "h_muon_0_iso_ptcone30", "h_muon_0_iso_ptcone30_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptcone30.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_iso_ptcone40, "h_muon_0_iso_ptcone40", "h_muon_0_iso_ptcone40_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptcone40.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_ptcone20, "h_muon_0_iso_ptcone20", "h_muon_0_iso_ptcone20_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptcone20_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_ptcone30, "h_muon_0_iso_ptcone30", "h_muon_0_iso_ptcone30_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptcone30_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_ptcone40, "h_muon_0_iso_ptcone40", "h_muon_0_iso_ptcone40_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptcone40_Zmumu2Jets.pdf", AnalysisType);
 
 	// topoetcone histograms
-	DrawHistogram(h_muon_0_iso_topoetcone20, "h_muon_0_iso_topoetcone20", "h_muon_0_iso_topoetcone20_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_topoetcone20.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_iso_topoetcone30, "h_muon_0_iso_topoetcone30", "h_muon_0_iso_topoetcone30_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_topoetcone30.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_iso_topoetcone40, "h_muon_0_iso_topoetcone40", "h_muon_0_iso_topoetcone40_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_topoetcone40.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_topoetcone20, "h_muon_0_iso_topoetcone20", "h_muon_0_iso_topoetcone20_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_topoetcone20_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_topoetcone30, "h_muon_0_iso_topoetcone30", "h_muon_0_iso_topoetcone30_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_topoetcone30_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_topoetcone40, "h_muon_0_iso_topoetcone40", "h_muon_0_iso_topoetcone40_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_topoetcone40_Zmumu2Jets.pdf", AnalysisType);
 	
 	// ptvarcone histograms
-	DrawHistogram(h_muon_0_iso_ptvarcone20, "h_muon_0_iso_ptvarcone20", "h_muon_0_iso_ptvarcone20_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptvarcone20.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_iso_ptvarcone40, "h_muon_0_iso_ptvarcone30", "h_muon_0_iso_ptvarcone30_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptvarcone30.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_iso_ptvarcone30, "h_muon_0_iso_ptvarcone40", "h_muon_0_iso_ptvarcone40_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptvarcone40.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_ptvarcone20, "h_muon_0_iso_ptvarcone20", "h_muon_0_iso_ptvarcone20_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptvarcone20_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_ptvarcone40, "h_muon_0_iso_ptvarcone30", "h_muon_0_iso_ptvarcone30_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptvarcone30_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_iso_ptvarcone30, "h_muon_0_iso_ptvarcone40", "h_muon_0_iso_ptvarcone40_Zmumu2Jets", "", 600, 400, true, "h_muon_0_iso_ptvarcone40_Zmumu2Jets.pdf", AnalysisType);
 	
 	// muon_0_p4_Pt
 	DrawHistogram(h_muon_0_p4_Pt, "h_muon_0_p4_Pt", "h_muon_0_p4_Pt_Zmumu2Jets", "", 600, 400, true, "h_muon_0_p4_Pt.pdf", AnalysisType);
 
 	// muon 0 & muon 1 histograms
-	DrawHistogram(h_muon_0_muon_1_mass_PRE, "h_muon_0_muon_1_mass_PRE", "h_muon_0_muon_1_mass_PRE_Zmumu2Jets", "Invariant Mass [GeV/c^{2}]", 600, 400, false, "h_muon_0_muon_1_mass_PRE.pdf", AnalysisType);
-	DrawHistogram(h_muon_0_muon_1_mass, "h_muon_0_muon_1_mass", "h_muon_0_muon_1_mass_Zmumu2Jets", "Invariant Mass [GeV/c^{2}]", 600, 400, false, "h_muon_0_muon_1_mass.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_muon_1_mass_PRE, "h_muon_0_muon_1_mass_PRE", "h_muon_0_muon_1_mass_PRE_Zmumu2Jets", "Invariant Mass [GeV/c^{2}]", 600, 400, false, "h_muon_0_muon_1_mass_PRE_Zmumu2Jets.pdf", AnalysisType);
+	DrawHistogram(h_muon_0_muon_1_mass, "h_muon_0_muon_1_mass", "h_muon_0_muon_1_mass_Zmumu2Jets", "Invariant Mass [GeV/c^{2}]", 600, 400, false, "h_muon_0_muon_1_mass_Zmumu2Jets.pdf", AnalysisType);
 
 	// Delta R Histograms
 	DrawHistogram(h_DeltaR_PRE, "h_DeltaR_PRE", "h_DeltaR_PRE_Zmumu2Jets", "", 600, 400, false, "h_DeltaR_PRE_muon.pdf", AnalysisType);
