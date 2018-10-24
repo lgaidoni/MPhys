e56513d7d6359c420f127e79a168c7dd9cc73198
AliceEMaffs <32435141+AliceEMaffs@users.noreply.github.com>
2018-10-24 18:31:37 +0100

    added combined pT of muons
    added combined pT of muons - I guess because ROOT crashed last session it didnt save :( 
    and put things in order of use for readability
    - if we add things add them to the END unless it is logical not to (I just get confused when I cant find things as they are muddled up)

--- Code/Headers/Histo_Book_Functions_Custom.h
+++ Code/Headers/Histo_Book_Functions_Custom.h
@@ -5,6 +5,8 @@
 /// ---------------------FORMATTING OF CUSTOM HISTOGRAM DEFINITIONS-----------------///
 ///////////////////////////////////////////////////////////////////////////////////////
 
+// BOOK KEEPING: please can we keep things in chronological order (of use, eg. pre before after cuts!)
+
 /// -- Virtual book function to book the histogram
 //void MC_Analysis::Book_variable(int bins, double min, double max) {
 //	h_variable = new TH1F("h_variable", "", bins, min, max);
@@ -38,56 +40,64 @@ void MC_Analysis::Book_DeltaR_PRE(int bins, double min, double max) {
 
 ///-------------Invariant mass of electron 0 and electron 1-----------------///
 
-//Electron 0 & Electron 1 Invariant Mass
-void MC_Analysis::Book_elec_0_elec_1_mass(int bins, double min, double max) {
-	h_elec_0_elec_1_mass = new TH1F("h_elec_0_elec_1_mass", "", bins, min, max);
-}
-
 //Electron 0 & Electron 1 Invariant Mass before cuts are made
 void MC_Analysis::Book_elec_0_elec_1_mass_PRE(int bins, double min, double max) {
 	h_elec_0_elec_1_mass_PRE = new TH1F("h_elec_0_elec_1_mass_PRE", "", bins, min, max);
 }
 
-///-------------Combined pT of electron 0 and electron 1-----------------///
-
-//Electron 0 & Electron 1 pT
-void MC_Analysis::Book_elec_0_elec_1_pt(int bins, double min, double max) {
-	h_elec_0_elec_1_pt = new TH1F("h_elec_0_elec_1_pt", "", bins, min, max);
+//Electron 0 & Electron 1 Invariant Mass
+void MC_Analysis::Book_elec_0_elec_1_mass(int bins, double min, double max) {
+	h_elec_0_elec_1_mass = new TH1F("h_elec_0_elec_1_mass", "", bins, min, max);
 }
+
+///-------------Combined pT of electron 0 and electron 1-----------------///
 
 //Electron 0 & Electron 1 pT before cuts are made
 void MC_Analysis::Book_elec_0_elec_1_pt_PRE(int bins, double min, double max) {
 	h_elec_0_elec_1_pt_PRE = new TH1F("h_elec_0_elec_1_pt_PRE", "", bins, min, max);
 }
 
+//Electron 0 & Electron 1 pT
+void MC_Analysis::Book_elec_0_elec_1_pt(int bins, double min, double max) {
+	h_elec_0_elec_1_pt = new TH1F("h_elec_0_elec_1_pt", "", bins, min, max);
+}
 
 /// ----------- MUONS ------------- ///
 
 ///-------------Invariant mass of muon 0 and muon 1-----------------///
-
-//muon 0 & muon 1 Invariant Mass
-void MC_Analysis::Book_muon_0_muon_1_mass(int bins, double min, double max) {
-	h_muon_0_muon_1_mass = new TH1F("h_muon_0_muon_1_mass", "", bins, min, max);
-}
 
 // muon 0 & muon 1 Invariant Mass before cuts are made
 void MC_Analysis::Book_muon_0_muon_1_mass_PRE(int bins, double min, double max) {
 	h_muon_0_muon_1_mass_PRE = new TH1F("h_muon_0_muon_1_mass_PRE", "", bins, min, max);
 }
 
-/// ---------- JETS --------------///
-
-//Function for booking leading jets invariant mass
-void MC_Analysis::Book_ljet_0_ljet_1_mass(int bins, double min, double max) {
-	h_ljet_0_ljet_1_mass = new TH1F("h_ljet_0_ljet_1_mass", "", bins, min, max);
+//muon 0 & muon 1 Invariant Mass
+void MC_Analysis::Book_muon_0_muon_1_mass(int bins, double min, double max) {
+	h_muon_0_muon_1_mass = new TH1F("h_muon_0_muon_1_mass", "", bins, min, max);
 }
+
+/// ---------- JETS --------------///
 
 //Function for booking leading jets invariant mass
 void MC_Analysis::Book_ljet_0_ljet_1_mass_PRE(int bins, double min, double max) {
 	h_ljet_0_ljet_1_mass_PRE = new TH1F("h_ljet_0_ljet_1_mass_PRE", "", bins, min, max);
 }
 
+//Function for booking leading jets invariant mass
+void MC_Analysis::Book_ljet_0_ljet_1_mass(int bins, double min, double max) {
+	h_ljet_0_ljet_1_mass = new TH1F("h_ljet_0_ljet_1_mass", "", bins, min, max);
+}
 
+///-------------Combined pT of muon 0 and muon 1-----------------///
 
+// muon 0 & muon 1 pT before cuts are made
+void MC_Analysis::Book_muon_0_muon_1_pt_PRE(int bins, double min, double max) {
+	h_muon_0_muon_1_pt_PRE = new TH1F("h_muon_0_muon_1_pt_PRE", "", bins, min, max);
+}
+
+// muon 0 & muon 1 pT
+void MC_Analysis::Book_muon_0_muon_1_pt(int bins, double min, double max) {
+	h_emuon_0_muon_1_pt = new TH1F("h_muon_0_muon_1_pt", "", bins, min, max);
+}
 
 #endif
