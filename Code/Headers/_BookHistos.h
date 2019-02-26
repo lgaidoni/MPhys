17832b93a8c2163dc68d228b63a30b6a3288e210
AliceEMaffs <AliceEMaffs@github.com>
2019-02-26 12:47:44 +0000

    added neutrino reconstruction stuff for Et outside and inside rapidity interval needs to be checked and correted for errors

--- Code/Headers/_BookHistos.h
+++ Code/Headers/_BookHistos.h
@@ -124,3 +124,9 @@
 	//Histogram Bookings for Test_Polar_Plot
 	Book_Test_Polar_Plot(xbins, Test_Polar_Plot_xMin, Test_Polar_Plot_xMax, ybins, Test_Polar_Plot_yMin, Test_Polar_Plot_yMax);
 
+	//Histogram Bookings for lep_0_reco_p4
+	Book_lep_0_reco_p4(xbins, lep_0_reco_p4_xMin, lep_0_reco_p4_xMax, ybins, lep_0_reco_p4_yMin, lep_0_reco_p4_yMax);
+
+	//Histogram Bookings for lep_1_reco_p4
+	Book_lep_1_reco_p4(xbins, lep_1_reco_p4_xMin, lep_1_reco_p4_xMax, ybins, lep_1_reco_p4_yMin, lep_1_reco_p4_yMax);
+
