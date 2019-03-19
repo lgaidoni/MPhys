de1dae4cb8b91a7e57090b471ae44ec319de2792
lgaidoni <lgaidoni@github.com>
2019-03-19 12:07:32 +0000

    Modifications to data transfer scripts, and started to add in Inside Outside Overlay function

--- data_from_share.sh
+++ data_from_share.sh
@@ -1,2 +1,5 @@
 cp -RP /pc2014-data4/alice-luca/Output-Files ~/
 cp -RP /pc2014-data4/alice-luca/Root-Files ~/
+cp -RP /pc2014-data4/alice-luca/Status_Files ~/
+cp -RP /pc2014-data4/alice-luca/Timing_Files ~/
+cp -RP /pc2014-data4/alice-luca/Completion_Files ~/
--- data_to_share.sh
+++ data_to_share.sh
@@ -1,2 +1,5 @@
 cp -RP ~/Output-Files /pc2014-data4/alice-luca/
 cp -RP ~/Root-Files /pc2014-data4/alice-luca/
+cp -RP ~/Status_Files /pc2014-data4/alice-luca/
+cp -RP ~/Timing_Files /pc2014-data4/alice-luca/
+cp -RP ~/Completion_Files /pc2014-data4/alice-luca/
