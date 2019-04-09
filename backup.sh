bf533e3af77474db7ed4263f40de1ef6b240ec88
lgaidoni <lgaidoni@github.com>
2019-04-09 18:29:44 +0100

    Minor changes to program in order to enhance higgs analysis

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 DATE=`date +%d-%m-%y_%H-%M-%S`
 echo "DATE = "$DATE
 
-cp -R ~/MPhys ~/Backup/MPhys/MPhys_$DATE
-cp -R ~/Output-Files ~/Backup/Output-Files/Output-Files_$DATE
-cp -R ~/Root-Files ~/Backup/Root-Files/Root-Files_$DATE
+cp -R ~/MPhys /pc2012-data1/alice-luca/LucaBackup/MPhys/MPhys_$DATE
+cp -R ~/Output-Files /pc2012-data1/alice-luca/LucaBackup/Output-Files/Output-Files_$DATE
+cp -R ~/Root-Files /pc2012-data1/alice-luca/LucaBackup/Root-Files/Root-Files_$DATE
